2b1d3789227fdceae623f71728f8a3811a3db962
Abigail Bruno <abigailbruno2005@gmail.com>
2023-07-18 22:39:15 -0300

    agrego tp3

--- Tareas/tareas3.js
+++ Tareas/tareas3.js
@@ -2,27 +2,26 @@
 // vocales que tiene la frase y muestre en consola cuales son.
 
 let frase= prompt("Ingrese su frase:");
-fraseMinuscula = frase.toLocaleLowerCase();
 let vocales= ["a", "e", "i","o","u","á","é","í", "ó", "ú"];
 let totalVocales = [];
 
+let letras = frase.toLocaleLowerCase().split("");
 if(!isNaN(frase)){
 	alert ("Introduzca una frase valida.");
 }
-for (let i = 0; i < fraseMinuscula.length; i++ ) {
-	let letra = fraseMinuscula[i];
+for (let i = 0; i < letras.length; i++ ) {
 	
-	if(vocales.includes(letra)) {
-		
 	
-	if(!totalVocales.includes(letra)){
-		totalVocales.push(letra);
+	if(vocales.includes(letras[i])) {
+		totalVocales.push(letras[i]);
 	}
-	}
+	
 }
 
 
-console.log("Su frase tiene:",length)
+console.log(`Su frase tiene: ${totalVocales.length} vocales y son: ${totalVocales}`)
+
+
 
 
 
--- TrabajosPracticos/tp3.js
+++ TrabajosPracticos/tp3.js
@@ -1,8 +1,69 @@
+// 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. 
+// Mostrar por pantalla el resultado devuelto por la función.
 
-let meses= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-for (let i = 0; i < meses.length; i++) {
+const resultado =function(numero){
+    if (!isNaN(numero)){
+    if (numero %2==0) {
+        alert("El número ingresado es par!")
+    }
+    else{
+        alert("El resultado es impar!")
+    }
+    }
+    } 
     
-    document.write (`Lista de meses:\n ${meses.join( )[i]}\n`);
 
+ //5- Definir una función que muestre información sobre una cadena de texto que se le pasa como 
+ //argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo
+ // por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+ const cadena = function (texto=prompt("Ingrese una cadena de texto:")) {
+    if(!isNaN(texto)){
+        return 'Ingresaste un numero! ingresa una cadena de texto.'
+    }else{
+        if (texto == texto.toUpperCase()) {
+            return "El texto contiene sólo mayúsculas."
+        } else if (texto == texto.toLowerCase()) {
+            return "El texto contiene sólo minúsculas."
+        } else if (texto == texto.toLowerCase().toLocaleUpperCase()) {
+            return "El texto contiene mayúsculas y minúsculas."
+        }
+    }
+    }
+
+
+
+//  6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, 
+//  luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+//  La fórmula del perímetro  es p = 2*(a +b)
+ 
+const calculodeperimetro = function( 
+    valor1 = Number(prompt('Ingrese el primer valor del rectángulo:')),
+    valor2 =Number(prompt('Ingrese el segundo valor del rectángulo:'))){
+    if(isNaN(valor1,valor2)){
+        return'Ingresa sólo números.'
+    }else {
+        let perimetro = (valor1 + valor2) * 2;
+        return `Su perímetro es: ${perimetro}`
+    }
+}
+
+
+
+//  7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+
+
+
+function tablaMultiplicacion (numero =parseInt.prompt("Ingrese el número a multiplicar:")){
+
+    if(isNaN(numero)){
+        return alert("Ingrese un número válido.")
+    }
+
+    document.write(`Tabla del ${numero} <br>`)
+for (let i = 1; i <= 10; i++) {
+    
+    document.write(`${numero} x ${i} = ${numero * i} <br>`)
+}
 }
